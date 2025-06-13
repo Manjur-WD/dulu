874c54293c10cd07936bcfd5c21ff51460469823
Manjur-WD <mak985812@gmail.com>
2025-06-14 00:56:59 +0530

    updated

--- js/custom_gsap.js
+++ js/custom_gsap.js
@@ -31,13 +31,75 @@ document.addEventListener("DOMContentLoaded", () => {
     ease: "back.out",
   });
 
-  // Remove this if you're triggering animation on scroll
-  // gsap.from("#hero", {
-  //   scale: 0,
-  //   y: 10,
-  //   delay: 1,
-  //   duration: 1,
-  //   ease: "back.out",
-  // });
+  gsap.to("#hero", {
+    scrollTrigger: {
+      trigger: "#hero",
+      start: "80% 50%",
+      end: "bottom 50%",
+      scrub: true,
+      // markers: true,
+    },
+    scale: 0.5,
+    rotate: -10,
+    ease: "none",
+
+  })
+
+  gsap.from("#about-us h2", {
+    scrollTrigger: {
+      trigger: "#about-us",
+      start: "top 60%",
+      end: "20% 60%",
+      // markers: true,
+    },
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: "back.out",
+  })
+  gsap.from("#about-us p", {
+    scrollTrigger: {
+      trigger: "#about-us",
+      start: "top 50%",
+      end: "30% 50%",
+      // markers: true,
+      scrub: true,
+    },
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: "back.out",
+  })
+  gsap.from(".all-products img", {
+    scrollTrigger: {
+      trigger: ".all-products",
+      start: "top 60%",
+      end: "30% 60%",
+      // markers: true,
+      scrub: true,
+    },
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2,
+    ease: "back.out",
+  })
+
+  gsap.from("#amra img", {
+    scrollTrigger: {
+      trigger: "#amra",
+      start: "top 80%",
+      end: "30% 80%",
+      // markers: true,
+      scrub: true,
+    },
+    y: 100,
+    scale: 0,
+    duration: 2,
+    stagger: 0.3,
+    ease: "back.out",
+  })
 
 });
